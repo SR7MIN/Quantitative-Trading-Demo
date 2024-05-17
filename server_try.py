e9ed562a2f7eb5ac97cd613a3367d038ad508cad
bingyang-lei <1257047642@qq.com>
2024-05-17 21:00:25 +0800

    解决冲突

--- server_try.py
+++ server_try.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # 创建数据库连接
 DATABSE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
-    user='root', password='123456', server='localhost', database='all_users')
+    user='root', password='123456', server='localhost', database='db_fin_tech')
 engine = create_engine(DATABSE_URI)
 # engine = create_engine('mysql://root:428099@114.212.81.66/mydb')
 # 创建会话工厂
@@ -16,36 +16,36 @@ Base = declarative_base()
 # 创建数据模型类
 
 
-class Student(Base):
-    __tablename__ = 'All_User_table'
+class Users(Base):
+    __tablename__ = 'users'
     account = Column(String(25), primary_key=True)
     name = Column(String(50))
     password = Column(String(10))
 
 
-# 创建表
-Base.metadata.create_all(engine)
+# # 创建表
+# Base.metadata.create_all(engine)
 
 # 创建会话
 session = Session()
 
 # 插入数据
-student1 = Student(account='1257047642', name="Alice", password="123")
-student2 = Student(account="1271060910",name='Bob',password="123")
-session.add_all([student1, student2])
-session.commit()
+student1 = Users(account='1257047642', name="", password="123456")
+# student2 = Users(account="1271060910",name='Bob',password="123")
+# session.add_all([student1, student2])
+# session.commit()
 
-# 查询数据
-students = session.query(Student).all()
-for student in students:
-    print(student.account, student.name, student.password)
+# # 查询数据
+# students = session.query(Users).all()
+# for student in students:
+#     print(student.account, student.name, student.password)
 
-# 更新数据
-student1.name = "Jack"
-session.commit()
+# # 更新数据
+# student1.name = "Jack"
+# session.commit()
 
 # 删除数据
-session.delete(student2)
+session.delete(student1)
 session.commit()
 
 # 关闭会话
